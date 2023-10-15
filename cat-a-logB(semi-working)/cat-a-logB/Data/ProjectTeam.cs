0f89248bcc292ef16e3f3628c20f0e076618c443
Marijonas <marijoniukas@gmail.com>
2023-10-15 22:37:21 +0300

    Updates to Teams page

--- cat-a-logB(semi-working)/cat-a-logB/Data/ProjectTeam.cs
+++ cat-a-logB(semi-working)/cat-a-logB/Data/ProjectTeam.cs
@@ -2,10 +2,20 @@ public class ProjectTeam
 {
    public string Color { get; set; }
    public string Name { get; set; }
+   public List<GanttData> Tasks { get; set; }
 
    public ProjectTeam(string color, string name)
    {
       Color = color;
       Name = name;
+      Tasks = new List<GanttData>();
+   }
+   public ProjectTeam()
+   {
+   }
+
+   public static void GetTasksForTeam(List<GanttData> allTasks, ProjectTeam team)
+   {
+      team.Tasks = allTasks.Where(task => task.Team.Name == team.Name).ToList();
    }
 }
