16c2963bd8b337fbc925c241bd1dccc807b4fdf4
Marijonas <marijoniukas@gmail.com>
2023-11-15 16:32:37 +0200

    Fixed milestone rerender

--- cat-a-logB(semi-working)/cat-a-logB/Data/ProjectMilestone.cs
+++ cat-a-logB(semi-working)/cat-a-logB/Data/ProjectMilestone.cs
@@ -4,38 +4,15 @@
     {
         public string Name { get; set; }
         public List<GanttData> Tasks { get; set; }
+        public string Color { get; set; }
 
-        public event Action OnTasksUpdated;
 
-        public void UpdateTasks(List<GanttData> updatedTasks)
+        public ProjectMilestone(string name, List<GanttData> tasks, string color)
         {
-            Tasks = updatedTasks;
-            OnTasksUpdated?.Invoke();
-            UpdateDotColors();
+            this.Name = name;
+            this.Tasks = tasks;
+            this.Color = color;
         }
-
-        private void UpdateDotColors()
-        {
-            // Add your logic to update dot colors here
-            // For example, iterate through tasks and update the colors based on completion status
-            foreach (var task in Tasks)
-            {
-                if (task.Progress == 100)
-                {
-                    task.PointColor = "green";
-                }
-                else
-                {
-                    task.PointColor = task.Team.Color;
-                }
-            }
-
-            // Trigger a re-render of the component
-            OnTasksUpdated?.Invoke();
-        }
-
-
-
         public ProjectMilestone(string name)
         {
             Name = name;
@@ -52,18 +29,24 @@
             Tasks = allTasks.Where(task => task.Name == Name).ToList();
         }
 
-        public double CompletedTasksPercentage
+        public void CalculateCompletedTasksPercentage()
         {
-            get
+            if (Tasks.Count == 0)
             {
-                if (Tasks.Count == 0)
-                    return 0;
-
-                int completedTasks = Tasks.Count(task => task.Progress == 100);
-                return (double)completedTasks / Tasks.Count * 100;
+                return;
             }
 
+            int completedTasks = Tasks.Count(task => task.Progress == 100);
+            double completedTasksPercentage = (double)completedTasks / Tasks.Count * 100;
+
+            if (completedTasksPercentage == 100)
+            {
+                Color = "green";
+            }
+
+            return;
         }
+
 
 
 
