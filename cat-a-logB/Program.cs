d95c6c4a1fbc28d8435b4d4b438b12de9eaf8e68
Zehritas <90518415+Zehritas@users.noreply.github.com>
2023-12-22 00:24:39 +0200

    Update Program.cs, api root change

--- cat-a-logB/Program.cs
+++ cat-a-logB/Program.cs
@@ -15,7 +15,7 @@ var connectionString = builder.Configuration.GetConnectionString("cat_a_logBCont
 
 builder.Services.AddHttpClient("ApiClient", client =>
 {
-    client.BaseAddress = new Uri("https://cat-a-logb20231112200531.azurewebsites.net/api");
+    client.BaseAddress = new Uri("https://cat-a-logb-api.azurewebsites.net/api");
     // Other configuration settings...
 });
 
